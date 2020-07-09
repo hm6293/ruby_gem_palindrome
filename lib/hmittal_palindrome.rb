30e03f4c16bed9df4a302918c93cc5ae9a0831a4
Himanshu Mittal <himanshu.m@greatlearning.in>
2020-07-09 11:19:45 +0530

    initialize repo

---
+++ lib/hmittal_palindrome.rb
@@ -0,0 +1,6 @@
+require "hmittal_palindrome/version"
+
+module HmittalPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hmittal_palindrome/version.rb
@@ -0,0 +1,3 @@
+module HmittalPalindrome
+  VERSION = "0.1.0"
+end
